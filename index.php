e2c4fc092528c7a01c98aceeb335600ba54fb14a
kraziegent <olumolupeter@gmail.com>
2018-06-20 13:04:56 +0100

    Fixing the contact and footer pages as well as modularising the header and footer.

--- index.php
+++ index.php
@@ -1,75 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" >
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <link rel="shortcut icon" href="favicon.ico">
-
-        <title>e-LogBook.com: Just trying to have some fun!</title>
-
-        <link href="css/bootstrap.css" rel="stylesheet">
-        <link href="css/main.css" rel="stylesheet">
-
-        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!--[if lt IE 9]-->
-        <script src="js/html5shiv.js"></script>
-        <script src="js/respond.min.js"></script>
-        <!--[endif] -->
-    </head>
-
-    <body>
-    
-        <div class="navbar navbar-inverse navbar-fixed-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">e-LogBook</a>
-                </div>
-                <div class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Home</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#contact">Contact</a></li>
-                        <li>
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown</a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li class="divider"></li>
-                                <li class="dropdown-header">Nav header</li>
-                                <li><a href="#">Seperated link</a></li>
-                                <li><a href="#">One more seperated link</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <form class="navbar-form navbar-right">
-                        <div class="form-group">
-                            <input type="text" placeholder="Email" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <input type="password" placeholder="Password" class="form-control">
-                        </div>
-                        <button type="submit" class="btn btn-success">Sign in</button>
-                    </form>
-                </div>
-                <!--/ .navbar-collapse -->
-            </div>
-        </div>
+<?php require_once __DIR__ . '/Pages/header.php'; ?>
 
         <div class="jumbotron">
             <div class="container">
-                <h1>Hello, World</h1>
+                <h1>Hi, Guys</h1>
 
-                <p>This is a template for a simple marketing or informational website. it includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique</p>
+                <p>This app is meant to be used by IT students in different organisations and schools to maintain an online log which can be accessed by both their school supervisor and current organisation supervisor, this will help streamline their Logbook approval process.</p>
 
-                <p><a class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
+                <p><a class="btn btn-primary btn-lg" href="Pages/about.php">Learn more &raquo;</a></p>
             </div>
         </div>
 
@@ -96,14 +33,4 @@
             </div>
 
             <hr>
-
-            <footer>
-                <p>&copy; AirPupnMeow.com</p>
-            </footer>
-        </div>
-        <!-- /container -->
-
-        <script src="js/jquery.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-    </body>
-</html>
+<?php require_once __DIR__ . '/Pages/footer.php'; ?>
